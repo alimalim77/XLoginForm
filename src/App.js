12b1c377b2780149ba0f63db417ac8765d5f3f4e
alimalim77 <alimalim77@gmail.com>
2024-03-08 12:36:08 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useRef } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [flag, setFlag] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    username === "user" && password === "password" ? setFlag(1) : setFlag(2);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Login Page</h1>
+      {flag === 0 ? (
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div>
+            <label htmlFor="username" name="username">
+              Username
+            </label>
+            <input
+              type="text"
+              name="username"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password" name="password">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              required
+            />
+          </div>
+
+          <button type="submit" name="login">
+            Submit
+          </button>
+        </form>
+      ) : flag === 1 ? (
+        "Welcome, user!"
+      ) : (
+        "Invalid username or password"
+      )}
     </div>
   );
 }
